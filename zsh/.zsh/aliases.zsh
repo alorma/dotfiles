467fb0c44eb7b862cfbdc8c1a36b676273f3961d
Bernat Borrás <bernat.borras@adevinta.com>
2025-01-28 13:37:26 +0100

    Thanks @Sloy for your commits

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -4,16 +4,6 @@ alias la="ls -lAFh"  # List all files (inlcuding hidden)
 alias lh="ls -ld .*" # List hidden files only
 alias lr="ls -tRFh"  # List recursively
 
-# zsh
-alias zshconfig="st ~/.zsh"
-alias localrc="if [[ -a ~/.localrc ]]; then ${EDITOR} ~/.localrc; fi"
-alias ohmyzsh="st ~/.oh-my-zsh"
-alias reload!='. ~/.zshrc'
-
-# terminal tricks
-alias cls='clear'
-alias j="jump"
-alias cpdir="cp -R"
 
 ## Hidden files in Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
@@ -22,19 +12,6 @@ alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall F
 ## Gradle
 alias gw='./gradlew'
 alias gwstop='./gradlew --stop'
-alias gwdependencies='./gradlew :app:dependencies --configuration debugCompileClasspath -q | st'
-alias gradleconfig="open ~/.gradle/gradle.properties"
-function taskTree() {
-  # https://github.com/dorongold/gradle-task-tree
-  if [ "$1" != "" ]
-  then
-    echo "Generating tree for task: $1"
-    gw "$1" taskTree --no-repeat -q | st
-    echo "Done!"
-  else
-    echo "You must provide a task as argument"
-  fi
-}
 
 # npm
 alias npmr='npm run'
@@ -48,7 +25,7 @@ alias meh='echo "¯\_(シ)_/¯" | pbcopy && echo "¯\_(シ)_/¯ copied"'
 #linux: alias meh='echo "¯\_(シ)_/¯" | xclip -selection c && echo "¯\_(シ)_/¯ copied"'
 
 ## Dotfiles self-awareness (aka skynet)
-alias dotfiles='code ~/dotfiles && cd ~/dotfiles'
+alias dotfiles='subl ~/dotfiles && cd ~/dotfiles'
 
 # Fzf + bat https://remysharp.com/2018/08/23/cli-improved
 alias preview="fzf --preview 'bat --color \"always\" {}'"
